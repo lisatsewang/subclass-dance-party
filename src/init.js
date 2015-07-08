f9b58c8ac03b026ff798918b46fe8a80b13b0f71
Hack Reactor Students <accounts@hackreactor.com>
2015-07-08 16:38:43 -0700

    Add blinky color dancer

--- src/init.js
+++ src/init.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-    var dancer = new BlinkyDancer(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
